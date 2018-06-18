4a5dbd1f4dc2c74314be49488e6acf535ce944a1
Tardis Xu <xiaoxubeii@gmail.com>
2018-06-18 12:25:06 +0800

    Add log.

--- pkg/network/allocator/server/ipallocator_server.go
+++ pkg/network/allocator/server/ipallocator_server.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net"
 
 	"golang.org/x/net/context"
@@ -16,6 +15,7 @@ import (
 	"github.com/TalkingData/hummingbird/pkg/network/allocator/ipallocator"
 	"github.com/TalkingData/hummingbird/pkg/network/allocator/service"
 	"github.com/TalkingData/hummingbird/pkg/storage/storagebackend"
+	"github.com/golang/glog"
 )
 
 var configPath = flag.String("config", "", "The ipallocator server config path")
@@ -28,15 +28,21 @@ type IPAllocatorServer struct {
 }
 
 func (s *IPAllocatorServer) AllocateNext(ctx context.Context, ip *ipallocatorservice.IP) (*ipallocatorservice.IP, error) {
+	glog.Infof("start to allocate ip for container %s", ip.ContainerID)
 	assignedIP, err := s.IPAllocator.AllocateNext()
+	glog.V(1).Infof("allocate ip successfully, ip is %v", assignedIP)
 	if err != nil {
+		glog.Errorf("allocate ip error: %v", err)
 		return nil, err
 	}
 
+	glog.V(1).Infof("start to register, ip is %v, container_id is %s", assignedIP, ip.ContainerID)
 	err = s.IPRegistry.Register(&assignedIP, ip.ContainerID)
 	if err != nil {
+		glog.Errorf("register ip error: %v", err)
 		return nil, err
 	}
+	glog.V(1).Infof("register ip successfully, ip is %v, container_id is %s", assignedIP, ip.ContainerID)
 
 	ipCIDR := net.IPNet{IP: assignedIP, Mask: s.IPAllocator.CIDR().Mask}
 	ipR := &ipallocatorservice.IP{Ip: ipCIDR.String()}
@@ -51,25 +57,33 @@ func (s *IPAllocatorServer) AllocateNext(ctx context.Context, ip *ipallocatorser
 
 		ipR.Gateway = config.Gateway
 	}
+	glog.Infof("allocate and register ip successfully, ip is %v", ipR)
 	return ipR, nil
 }
 
 func (s *IPAllocatorServer) Release(ctx context.Context, ip *ipallocatorservice.IP) (*ipallocatorservice.Blank, error) {
+	glog.Infof("start to release ip, ip is %v", ip)
 	ipReg, err := s.IPRegistry.GetIP(ip.ContainerID)
 	if err != nil {
+		glog.Errorf("fail to get ip %v, error is %v", ip, err)
 		return nil, err
 	}
 
+	glog.V(1).Infof("start to deregister ip %v", ipReg)
 	err = s.IPRegistry.Deregister(ip.ContainerID)
 	if err != nil {
+		glog.Errorf("fail to deregister ip %v, error is %v", ip, err)
 		return nil, err
 	}
 
+	glog.V(1).Infof("start release ip %v", ipReg)
 	err = s.IPAllocator.Release(*ipReg)
+	glog.V(1).Infof("release ip successfully, ip is %v", ipReg)
 	if err != nil {
 		return nil, err
 	}
 
+	glog.V(1).Infof("release and deregister ip successfully, ip is %v", ip)
 	return &ipallocatorservice.Blank{}, nil
 }
 
@@ -128,22 +142,25 @@ func initConfig(configPath string) error {
 
 func main() {
 	flag.Parse()
+	defer glog.Flush()
+
 	err := initConfig(*configPath)
 	if err != nil {
-		log.Fatalf("failted to load config:%v", err)
+		glog.Fatalf("fail to load config %v", err)
 	}
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", config.Port))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		glog.Fatalf("fail to listen %v", err)
 	}
 	grpcServer := grpc.NewServer()
 	server := &IPAllocatorServer{}
 	err = newServer(config, server)
 	if err != nil {
-		log.Fatalf("failed to start server: %v", err)
+		glog.Fatalf("fail to start server %v", err)
 	}
 
 	ipallocatorservice.RegisterIPAllocatorServer(grpcServer, server)
+	glog.Infof("start server on port %d", config.Port)
 	grpcServer.Serve(lis)
 }

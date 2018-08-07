639260c94f4c70378044cfc9834208924f52e20b
Tardis Xu <xiaoxubeii@gmail.com>
2018-08-07 11:24:16 +0800

    Add dockerfile and fix wrong go path.

--- tools/spark-watcher/build.sh
+++ tools/spark-watcher/build.sh
@@ -2,7 +2,7 @@
 image="spark-watcher:$1"
 repo_server=$2
 
-go build -o spark-watcher github.com/TalkingData/hummingbird/server/starter.go
+go build -o spark-watcher $GOPATH/src/github.com/TalkingData/hummingbird/server/starter.go
 
 docker build -t $image .
 docker tag $image $repo_server/$image

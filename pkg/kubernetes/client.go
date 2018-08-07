2ec992894e6ccaeded9f67cc98228d983e93216e
Tardis Xu <xiaoxubeii@gmail.com>
2018-08-07 11:45:45 +0800

    Add kubernetes serviceaccount support.

--- pkg/kubernetes/client.go
+++ pkg/kubernetes/client.go
@@ -2,14 +2,24 @@ package kubernetes
 
 import (
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func GetClient(kubeconfig string) (*kubernetes.Clientset, error) {
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		return nil, err
+	var config *rest.Config
+	var err error
+	if kubeconfig != "" {
+		// use the current context in kubeconfig
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	// create the clientset

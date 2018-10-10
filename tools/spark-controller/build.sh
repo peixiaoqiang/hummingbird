cc188e4cc37b5dcf993770fc6d65dab347c32d83
Tim Xu <xiaoxubeii@gmail.com>
2018-10-10 15:23:11 +0800

    Add automatic build process.

--- tools/spark-controller/build.sh
+++ tools/spark-controller/build.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 image="spark-controller:$1"
 repo_server=$2
+src_path=$GOPATH/src/github.com/TalkingData/hummmingbird
 
-go build -o spark-controller $GOPATH/src/github.com/TalkingData/hummingbird/spark/starter.go
+function preinstall {
+    rm -rf src.tgz
+    current_path=$PWD
+    cd $src_path
+    tar czvf src.tgz .
+    mv src.tgz $current_path
+    cd $current_path
+}
 
-docker build -t $image .
-docker tag $image $repo_server/$image
-docker push $repo_server/$image
+function build_image {
+    docker build -t $image .
+    docker tag $image $repo_server/$image
+    docker push $repo_server/$image
+}   
